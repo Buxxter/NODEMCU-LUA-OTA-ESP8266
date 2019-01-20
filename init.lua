1c246d9ec94752b4627d8b790c88c59213e0f475
buxxter <buxxter@mail.ru>
2019-01-20 21:08:25 +0300

    Debug timer improved

--- init.lua
+++ init.lua
@@ -1,11 +1,16 @@
+function run()
+    print("Starting...")
+    if file.open("init_ota.lc") then
+        file.close()
+        dofile("init_ota.lc")
+    else
+        dofile("init_ota.lua")
+    end
+end
+
 if file.open("debug") then
     print("Debug delay for 3s...")
-    tmr.delay(3000000)
-    print("finished")
-end
-if file.open("init_ota.lc") then
-    file.close()
-    dofile("init_ota.lc")
+    tmr.create():alarm(3000, tmr.ALARM_SINGLE, run)
 else
-    dofile("init_ota.lua")
+    run()
 end
